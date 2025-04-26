d832353f958f5bbe84fe9060dd15c1cab3a0279d
Jadamal Mahendra <jadamalmahendra@gmail.com>
2025-04-26 19:00:21 +0530

    chore: Remove package-lock.json and update package.json and vite.config.js for dependency upgrades; add yarn.lock for Yarn support

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,5 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
-import { nodePolyfills } from 'vite-plugin-node-polyfills'
-import path from 'path'; // Import path for alias
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -11,7 +9,6 @@ export default defineConfig(({ mode }) => {
   return {
     plugins: [
       react(),
-      nodePolyfills(),
     ],
     server: {
       port: env.VITE_PORT || 3001, // use env variable or default to 3001
