f70204bc42ee0a9815413377aa7686d3caccd866
Jadamal Mahendra <jadamalmahendra@gmail.com>
2025-04-28 14:30:14 +0530

    refactor: Update AppLayout import in App.tsx to remove .tsx extension for consistency

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ import Home from './pages/Home';
 import BlogListPage from './pages/BlogListPage';
 import BlogPostPage from './pages/BlogPostPage';
 // Import the new layout component
-import AppLayout from "./layouts/AppLayout.tsx"; 
+import AppLayout from "./layouts/AppLayout"; 
 
 // Import base CSS
 import "./styles/index.css";
