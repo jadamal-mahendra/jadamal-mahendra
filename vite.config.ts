fda0e2343986cca282953e6c35debf0d5a8e933c
Jadamal Mahendra <jadamalmahendra@gmail.com>
2025-04-28 13:15:20 +0530

    chore: Update Vite configuration to include path aliasing for improved module resolution

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import path from 'path' // Import path module
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -13,6 +14,11 @@ export default defineConfig(({ mode }) => {
       tsconfigPaths(),
       react()
     ],
+    resolve: { // Add resolve section
+      alias: {
+        '@': path.resolve(__dirname, './src'), // Define @ alias
+      },
+    },
     server: {
       port: parseInt(env.VITE_PORT || '3001', 10), // Parse port to number
       open: true, // automatically open the browser
